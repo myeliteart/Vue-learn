5abae0274e610c532f693630eaaa3c0625b799da
tinega collins <84614751+tinegaCollins@users.noreply.github.com>
2024-01-01 22:16:41 +0300

    bootsrapp added

--- vue-website/src/App.vue
+++ vue-website/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button class="btn btn-primary">Primary button</button>
+    </div>
    <navBar></navBar>
   </div>
 </template>
--- vue-website/src/main.js
+++ vue-website/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/styles/styles.scss'
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
